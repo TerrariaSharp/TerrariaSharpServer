f7098b49f9932292272ad45e18617be5ea5ed2a6
valdgeorgio <edgekillerfr@live.fr>
2015-06-28 16:48:29 +0200

    first upload

---
+++ TerrariaSharpServer/TSS-ServerExample/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using TSSLib;
+
+namespace TSS_ServerExample
+{
+    class Program
+    {
+        private static TerrariaServer terrariaServer;
+
+        static void Main(string[] args)
+        {
+
+            terrariaServer = new TerrariaServer("config.cfg");
+            terrariaServer.Start();
+        
+        }
+    }
+}
---
+++ TerrariaSharpServer/TSSLib/TerrariaServer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSSLib
+{
+    public class TerrariaServer
+    {
+
+        public TerrariaServer(string configFilePath)
+        {
+
+        }
+
+        public void Start()
+        {
+
+        }
+
+    }
+}
